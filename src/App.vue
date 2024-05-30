47dcc92244b3390afbbdf3d81b01ff3618c37884
mattemutti <mattemutti@gmail.com>
2024-05-30 16:45:07 +0200

    update app.vue file and add api call for the  data in the back-end project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+
+export default {
+
+  name: 'App',
+
+
+  data() {
+    return {
+      message: 'By',
+      base_api_url: 'http://127.0.0.1:8000',
+      base_project_url: '/api/projects',
+      projects: [],
+      loading: true,
+
+
+    }
+  },
+  methods: {
+
+
+    callApi(url) {
+
+      axios.get(url)
+        .then(response => {
+          console.log(response);
+          this.projects = response.data.projects;
+          this.loading = false;
+        })
+        .catch(err => {
+          console.errol(err);
+        })
+    }
+  },
+  mounted() {
+
+    let url = this.base_api_url + this.base_project_url;
+    this.callApi(url);
+
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <h1>{{ message }}</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+* {
+  border: 1px dashed black;
 }
 </style>
