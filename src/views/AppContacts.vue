efffc74798d5023430759c88d38f1d192c92bfc7
mattemutti <mattemutti@gmail.com>
2024-07-30 09:32:03 +0200

    update mail

--- src/views/AppContacts.vue
+++ src/views/AppContacts.vue
@@ -10,26 +10,46 @@ export default {
 			name: '',
 			email: '',
 			message: '',
+			loading: false,
+			success: false,
+			errors: {},
 
 		}
 	},
 	methods: {
 		sendMessage() {
+			this.loading = true;
 			const data = {
 				name: this.name,
 				email: this.email,
-				message: this.message,
-			}
+				message: this.message
+			};
 
-			const apiUrl = `${this.base_api_url}/api/contacts`;
+			this.errors = {};
+
+			const apiUrl = `${this.base_api_url}/api/contacts`
 
 			axios.post(apiUrl, data)
-				.then(response => {
-					console.log(response);
+				.then(resp => {
+					console.log(resp);
+
+					if (resp.data.success) {
+						this.name = ''
+						this.email = ''
+						this.message = ''
+						this.success = resp.data.message
+
+					} else {
+						this.errors = resp.data.errors
+					}
+
+					this.loading = false;
 				})
 				.catch(err => {
 					console.error(err);
 				})
+
+
 		}
 	}
 }
@@ -45,25 +65,32 @@ export default {
 				<form @submit.prevent="sendMessage()">
 
 					<div class="mb-3">
-
-						<input type="name" class="input_box" name="name" id="name" aria-describedby="nameHelpId"
-							placeholder=" Nome" v-model="name" />
-						<!-- <small id="nameHelpId" class="form-text txt_color">Inserisci il tuo nome</small> -->
+						<input type="name" class="input_box" name="name" id="name"
+							:class="{ 'is-invalid': errors.name }" aria-describedby="nameHelpId" placeholder=" Nome"
+							v-model="name" />
+						<p v-for="(error, index) in errors.name" :key="`message-error-${index}`" class="">
+							Il nome è obbligatorio
+						</p>
 					</div>
-
 					<div class="mb-3 py-4">
-
-						<input type="email" class="input_box" name="email" id="email" aria-describedby="emailHelpId"
-							placeholder=" name@gmail.com" v-model="email" />
-						<!-- <small id="emailHelpId" class="form-text txt_color">Inserisci la tua email</small> -->
+						<input type="email" class="input_box" name="email" id="email"
+							:class="{ 'is-invalid': errors.email }" aria-describedby="emailHelpId"
+							placeholder=" nome@gmail.com" v-model="email" />
+						<p v-for="(error, index) in errors.email" :key="`message-error-${index}`" class="">
+							L'email è obbligatoria
+						</p>
 					</div>
 
 					<div class="mb-3">
 						<textarea class="text_box" name="message" id="message" cols="40" rows="6"
-							placeholder=" Scrivi il tuo messaggio" v-model="message"></textarea>
+							:class="{ 'is-invalid': errors.message }" placeholder=" Scrivi il tuo messaggio"
+							v-model="message"></textarea>
+						<p v-for="(error, index) in errors.message" :key="`message-error-${index}`" class="">
+							Il messaggio è obbligatorio
+						</p>
 					</div>
 
-					<button class="btn btn-dark" type="submit">Send</button>
+					<button class="btn btn-dark" type="submit" :disabled="loading">Invia</button>
 				</form>
 			</div>
 		</div>
